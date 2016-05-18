b17928cc47720d93a5f43f8bda8fee5d3455e56c
Chen Jin <chenjin@kingsoft.com>
2016-05-18 18:41:43 +0800

    增加从服务器端获取token示例

--- KS3SDKDemo/KS3SDKDemo-Token/KS3SDKDemo-TokenTests/KS3SDKDemo_TokenTests.m
+++ KS3SDKDemo/KS3SDKDemo-Token/KS3SDKDemo-TokenTests/KS3SDKDemo_TokenTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "../../../Framework/StaticFramework/KS3YunSDK.framework/Headers/KS3YunSDK.h"
 
 @interface KS3SDKDemo_TokenTests : XCTestCase
 
@@ -36,5 +37,45 @@
         // Put the code you want to measure the time of here.
     }];
 }
+
+- (void)testLisetBuckets
+{
+    KS3ListBucketsRequest *listBucketRequest = [[KS3ListBucketsRequest alloc] init];
+    NSDictionary *dicParams = [NSDictionary dictionaryWithObjectsAndKeys:
+                               listBucketRequest.httpMethod,  @"http_method",
+                               listBucketRequest.contentMd5,  @"content_md5",
+                               listBucketRequest.contentType, @"content_type",
+                               listBucketRequest.strDate,     @"date",
+                               listBucketRequest.kSYHeader,   @"headers",
+                               listBucketRequest.kSYResource, @"resource", nil];
+    NSURL *tokenUrl = [NSURL URLWithString:@"http://127.0.0.1:3000/token"];
+    NSMutableURLRequest *tokenRequest = [[NSMutableURLRequest alloc] initWithURL:tokenUrl
+                                                                     cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
+                                                                 timeoutInterval:10];
+    NSData *dataParams = [NSJSONSerialization dataWithJSONObject:dicParams options:NSJSONWritingPrettyPrinted error:nil];
+    [tokenRequest setURL:tokenUrl];
+    [tokenRequest setHTTPMethod:@"POST"];
+    [tokenRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    [tokenRequest setHTTPBody:dataParams];
+    [NSURLConnection sendAsynchronousRequest:tokenRequest queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        if (connectionError == nil) {
+            NSString *strToken = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            NSLog(@"#### 获取token成功! #### token: %@", strToken);
+            XCTAssertNotNil(strToken);
+            
+            listBucketRequest.strKS3Token = strToken;
+            NSArray *bucketArray = [[KS3Client initialize] listBuckets:listBucketRequest];
+            for (KS3Bucket *bucket in bucketArray ) {
+                NSLog(@"%@",bucket.creationDate);
+                NSLog(@"%@",bucket.name);
+            }
+        }
+        else {
+            NSLog(@"#### 获取token失败，error: %@", connectionError);
+        }
+    }];
+}
+
 
 @end
